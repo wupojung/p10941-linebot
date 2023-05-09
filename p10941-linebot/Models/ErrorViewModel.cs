09a3675338aa3f627975f65325b03cb9677f933b
Po Lung Wu <wupojung@gmail.com>
2023-05-09 15:33:17 +0800

    init

---
+++ p10941-linebot/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace p10941_linebot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
